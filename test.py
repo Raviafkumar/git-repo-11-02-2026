26191a021f263d0cbc4e973ba6073f6ac50943cb
ravi kumar <ravi.af.kumar@gmail.com>
2026-02-19 13:12:45 +0530

    updated workflow file

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import json
 import sys
+import ast
 with open('output.json','r') as f:
     output = json.load(f) 
+test_cases = ast.literal_eval(output['test_case_result']['result'])    
 
-for test_case in output['test_case_result']['result']:
+for test_case in test_cases:
     if test_case['test case result'] =='failed':
         print(f"Below test case failed: {test_case['test case desc']}")
         sys.exit(1)
